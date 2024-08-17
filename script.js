8ae63a65ccd55abd352386d4702b48562edfa1bf
unknown <examp23.lo23@gmail.com>
2024-08-17 22:27:24 +0800

    initial calculator commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const buttonsEl = document.querySelectorAll("button");
+const inputFieldel = document.getElementById("result");
+//let equalPress = false;
+
+for (let i = 0; i < buttonsEl.length; i++) {
+  buttonsEl[i].addEventListener("click", () => {
+    const buttonValue = buttonsEl[i].textContent;
+
+    if (buttonValue == "C") {
+      clearResult();
+    } else if (buttonValue == "=") {
+      //equalPress = true;
+      calculateResult();
+    } else {
+      appendValue(buttonValue);
+    }
+  });
+}
+
+function clearResult() {
+  inputFieldel.value = "";
+}
+
+function calculateResult() {
+  inputFieldel.value = eval(inputFieldel.value);
+  //inputFieldel.value = "I miss u :((";
+}
+
+function appendValue(buttonValue) {
+  inputFieldel.value += buttonValue;
+}
